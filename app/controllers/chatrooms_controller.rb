9e5b3557203896df755acc2915b7693449088b72
andres hernandez <aahrodriguez@gmail.com>
2021-06-11 21:08:51 -0300

    page and backend messages without action cable done

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,5 @@
 class ChatroomsController < ApplicationController
   def index
+    @messages = Message.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_192824) do
+ActiveRecord::Schema.define(version: 2021_06_11_233605) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "user_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
