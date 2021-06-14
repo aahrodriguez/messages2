5bbc50191748c2f153c5b6786ac5dfdbcd051e6a
Jonathan Barrios <jonathanbarrios1998@gmail.com>
2021-06-14 18:58:08 -0400

    Te falto renderizar
    A lo legal, ni idea que es

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ before_action :require_user
     message = current_user.messages.build(messages_param)
     if message.save
       puts "parte 1"
-      ActionCable.server.broadcast "chatroom_channel", foo: message.body
+      ActionCable.server.broadcast "chatroom_channel", foo: message_render( message.body || "Default Message" )
     else
       puts "parte 2"
       render :json {message: "error"}, status: 202
@@ -16,5 +16,9 @@ before_action :require_user
 
   def messages_param
     params.require(:message).permit(:body)
+  end
+  
+  def message_render(message)
+    render(partial: 'message', locals: {message: message})
   end
 end
